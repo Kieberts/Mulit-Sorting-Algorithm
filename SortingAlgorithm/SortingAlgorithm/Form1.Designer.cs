f3088d7602bda06775f5ea988365e7ebc7f77906
Kieberts <65255153+Kieberts@users.noreply.github.com>
2020-06-25 16:14:45 +0200

    Add files via upload

---
+++ SortingAlgorithm/SortingAlgorithm/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SortingAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSortAlge = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pcbShowArray = new System.Windows.Forms.PictureBox();
+            this.pcbTestButton = new System.Windows.Forms.PictureBox();
+            this.lblTestTrackBar = new System.Windows.Forms.Label();
+            this.trackBarSizeArray = new System.Windows.Forms.TrackBar();
+            this.BtnQuickSort = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbShowArray)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbTestButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSizeArray)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSortAlge
+            // 
+            this.btnSortAlge.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnSortAlge.Location = new System.Drawing.Point(165, 576);
+            this.btnSortAlge.Name = "btnSortAlge";
+            this.btnSortAlge.Size = new System.Drawing.Size(157, 35);
+            this.btnSortAlge.TabIndex = 2;
+            this.btnSortAlge.Text = "Sorting algorithm";
+            this.btnSortAlge.UseVisualStyleBackColor = true;
+            this.btnSortAlge.Click += new System.EventHandler(this.btnSortAlge_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(491, 576);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 35);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(654, 576);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(157, 35);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // pcbShowArray
+            // 
+            this.pcbShowArray.Location = new System.Drawing.Point(190, 170);
+            this.pcbShowArray.Name = "pcbShowArray";
+            this.pcbShowArray.Size = new System.Drawing.Size(600, 400);
+            this.pcbShowArray.TabIndex = 6;
+            this.pcbShowArray.TabStop = false;
+            this.pcbShowArray.Click += new System.EventHandler(this.pcbShowArray_Click);
+            this.pcbShowArray.Paint += new System.Windows.Forms.PaintEventHandler(this.pcbShowArray_Paint);
+            // 
+            // pcbTestButton
+            // 
+            this.pcbTestButton.Location = new System.Drawing.Point(190, 46);
+            this.pcbTestButton.Name = "pcbTestButton";
+            this.pcbTestButton.Size = new System.Drawing.Size(610, 85);
+            this.pcbTestButton.TabIndex = 7;
+            this.pcbTestButton.TabStop = false;
+            // 
+            // lblTestTrackBar
+            // 
+            this.lblTestTrackBar.AutoSize = true;
+            this.lblTestTrackBar.Location = new System.Drawing.Point(46, 640);
+            this.lblTestTrackBar.Name = "lblTestTrackBar";
+            this.lblTestTrackBar.Size = new System.Drawing.Size(35, 13);
+            this.lblTestTrackBar.TabIndex = 9;
+            this.lblTestTrackBar.Text = "label1";
+            // 
+            // trackBarSizeArray
+            // 
+            this.trackBarSizeArray.Location = new System.Drawing.Point(165, 640);
+            this.trackBarSizeArray.Name = "trackBarSizeArray";
+            this.trackBarSizeArray.Size = new System.Drawing.Size(646, 45);
+            this.trackBarSizeArray.TabIndex = 10;
+            this.trackBarSizeArray.Scroll += new System.EventHandler(this.trackBarSizeArray_Scroll);
+            // 
+            // BtnQuickSort
+            // 
+            this.BtnQuickSort.Location = new System.Drawing.Point(328, 576);
+            this.BtnQuickSort.Name = "BtnQuickSort";
+            this.BtnQuickSort.Size = new System.Drawing.Size(157, 35);
+            this.BtnQuickSort.TabIndex = 11;
+            this.BtnQuickSort.Text = "Quick Sort";
+            this.BtnQuickSort.UseVisualStyleBackColor = true;
+            this.BtnQuickSort.Click += new System.EventHandler(this.BtnQuickSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 711);
+            this.Controls.Add(this.BtnQuickSort);
+            this.Controls.Add(this.trackBarSizeArray);
+            this.Controls.Add(this.lblTestTrackBar);
+            this.Controls.Add(this.pcbTestButton);
+            this.Controls.Add(this.pcbShowArray);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnSortAlge);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbShowArray)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbTestButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSizeArray)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSortAlge;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pcbShowArray;
+        private System.Windows.Forms.PictureBox pcbTestButton;
+        private System.Windows.Forms.Label lblTestTrackBar;
+        private System.Windows.Forms.TrackBar trackBarSizeArray;
+        private System.Windows.Forms.Button BtnQuickSort;
+    }
+}
+
---
+++ SortingAlgorithm/SortingAlgorithm/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        #region Variables;
+        // A few Variables for the Application
+        public Graphics g;
+        public Random rnd = new Random();
+        public int[] test;
+        public int k = -1;
+        public int trackArraySize = 5;
+        public int speedBubbleSort = 405;
+        public SolidBrush brush = new SolidBrush(Color.Red);
+        public int highestNumberholderBubble = -1;
+        public int holderQuickSort = -1;
+        public int num = -1;
+        #endregion
+
+
+        #region Form Loads
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // disables the buttons 
+            btnSortAlge.Enabled = false;
+            BtnQuickSort.Enabled = false;
+
+
+            // Sets the Min and Max for the Array
+            this.trackBarSizeArray.Maximum = 105;
+            this.trackBarSizeArray.Minimum = 5;
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        #endregion
+
+        private void BtnQuickSort_Click(object sender, EventArgs e)
+        {
+            // Calls Quick Sort Algorithm
+            QuickSort(test);
+            k = 0;
+            pcbShowArray.Refresh();
+        }
+        private void btnSortAlge_Click(object sender, EventArgs e)
+        {
+            // Calles Sort Algorithm
+            BubbleSort();
+            k = 0;
+            pcbShowArray.Refresh();
+        }
+        private void pcbShowArray_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            
+            // k said which button you have Pressed and it will call the right Methode for it
+            
+
+            if (k == 0)
+            {
+                // 
+
+                ShowArray();
+            }
+            else if (k == 1)
+            {
+            }
+            
+
+        }
+        private void pcbShowArray_Click(object sender, EventArgs e)
+        {
+            // Each time you click the pictureBox, it will give you the Location of the Mouse Cursor
+            // And will show you the numbers in a MessageBox
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point mouse = me.Location;
+            
+            MessageBox.Show(mouse.X + " " + mouse.Y);
+        }
+
+
+        #region Methodes
+        private void CreateArray()
+        {
+            // Creates Array with Size of TrackBar Value and gives each one a random Number
+            test = new int[trackArraySize];
+            for (int i = 0; i < test.Length; i++)
+            {
+                test[i] = rnd.Next(5, 350);
+            }
+        }
+        private void ShowArray()
+        {
+            // Creates the visual Bars in PictureBox
+            // Each Bar has the height of the Array number
+            
+            int x = 0;
+            int sizeX = 600 / test.Length;
+            int width = (600 / test.Length) / 2;
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                if (i == highestNumberholderBubble || i == num)
+                {
+                    brush = new SolidBrush(Color.Blue);
+                }
+                g.FillRectangle(brush, 5 + x, 0, width, test[i]);
+                brush = new SolidBrush(Color.Red);
+                x += sizeX;
+
+            }
+        }
+
+
+        #region Sorting Algorithm
+        private void BubbleSort()
+        {
+
+            // This is the Methode to sort an array
+
+            int i, j;
+            int N = test.Length;
+
+            for (j = N - 1; j > 0; j--)
+            {
+                for (i = 0; i < j; i++)
+                {
+                    if (test[i] > test[i + 1])
+                    {
+                        highestNumberholderBubble = i + 1;
+                        exchange(test, i, i + 1);
+                        Thread.Sleep(speedBubbleSort);
+
+                        pcbShowArray.Refresh();
+                        
+                    }
+                       
+                }
+            }
+        }
+
+
+        private void QuickSort(int[] array)
+        {
+            QuickSort(array, 0, array.Length - 1);
+
+        }
+
+        private void QuickSort(int[] array, int start, int ende)
+        {
+            if (start >= ende)
+            {
+                return;
+            }
+
+            num = array[start];
+            int k = start;
+            int l = ende;
+
+            while (k < l)
+            {
+                while (k < l && array[l] >= num)
+                {
+                    l--;
+                }
+
+                array[k] = array[l];
+
+                while (k < l && array[k] < num)
+                {
+                    k++;
+                }
+
+                array[l] = array[k];  
+
+            }
+            
+            
+            array[k] = num;
+            Thread.Sleep(50);
+            pcbShowArray.Refresh();
+            QuickSort(array, start, k - 1);
+            QuickSort(array, k + 1, ende);
+
+        }
+
+        #endregion
+        private static void exchange(int[] data, int x , int y)
+        {
+            int temporary;
+
+            temporary = data[x];
+            data[x] = data[y];
+            data[y] = temporary;
+
+        }
+
+
+
+
+
+
+
+
+
+        #endregion
+
+        private void trackBarSizeArray_Scroll(object sender, EventArgs e)
+        {
+            // Enables all buttons
+            btnSortAlge.Enabled = true;
+            BtnQuickSort.Enabled = true;
+            // Creates Array with the size of TrackBar Value
+            k = 0;
+            lblTestTrackBar.Text = "" + trackBarSizeArray.Value;
+            trackArraySize = trackBarSizeArray.Value;
+
+            #region Speed Value
+            if (trackBarSizeArray.Value <= 15)
+            {
+                speedBubbleSort = 200;
+            }
+            else if (trackBarSizeArray.Value >= 15 && trackBarSizeArray.Value < 25)
+            {
+                speedBubbleSort = 100;
+            }
+            else if (trackBarSizeArray.Value >= 25 && trackBarSizeArray.Value < 35)
+            {
+                speedBubbleSort = 60;
+            }
+            else if (trackBarSizeArray.Value >= 35 && trackBarSizeArray.Value < 45)
+            {
+                speedBubbleSort = 40;
+            }
+            else if (trackBarSizeArray.Value >= 45 && trackBarSizeArray.Value < 55)
+            {
+                speedBubbleSort = 20;
+            }
+            else if (trackBarSizeArray.Value >= 55 && trackBarSizeArray.Value < 65)
+            {
+                speedBubbleSort = 17;
+            }
+            else if (trackBarSizeArray.Value >= 65 && trackBarSizeArray.Value < 75)
+            {
+                speedBubbleSort = 14;
+            }
+            else if (trackBarSizeArray.Value >= 75 && trackBarSizeArray.Value < 85)
+            {
+                speedBubbleSort = 11;
+            }
+            else if (trackBarSizeArray.Value >= 85 && trackBarSizeArray.Value < 95)
+            {
+                speedBubbleSort = 8;
+            }
+            else
+            {
+                speedBubbleSort = 5;
+            }
+            #endregion
+
+            CreateArray();
+            pcbShowArray.Refresh();
+            
+        }
+
+        
+    }
+}
